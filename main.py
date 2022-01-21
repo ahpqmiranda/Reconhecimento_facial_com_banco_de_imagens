364b53145e45d0d5eb44e33ee7e4cdac5bb90009
Alan Miranda <51090438+Ahmiranda@users.noreply.github.com>
2022-01-21 10:45:03 -0300

    o código tá rodando certinho2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image
 import face_recognition as fr  # abreviei o nome da biblioteca para fr
 
 # carregando um arquivo de imagem
-imagem = fr.load_image_file("pessoas-conhecidas/alan2wanessa2.jpg")
+imagem = fr.load_image_file("pessoas-conhecidas/alan3wanessa3.jpg")
 # converte a imagem em vetor (R4)
 faces = fr.face_locations(imagem)
 # cada vetor é uma pessoa encontrada, uso a função len para contar quantos vetores tem
@@ -12,7 +12,7 @@ print('Foram encontradas {} pessoas'.format(len(faces)))
 
 # esse loop vai realizar o processo de 0 a n vetores encontrados em "faces"
 for coordenadas in faces:
-    top, right, bottom, left = coordenadas  # separo as coordenadas do vetor em variáveis
+    top, right, bottom, left = coordenadas  # separo as coordenadas do vetor em variáveis (as variáveis precisam ser chamadas de top, right, bottom, left ou não funciona, ele não chama a biblioteca api)
     rosto = imagem[top:bottom, left:right]  # monto uma box com dimensões equivalentes aos rostos encontrados
     recorte = Image.fromarray(rosto)  # recorta a imagem original com a box na região do rosto da pessoa
     recorte.show()  # mostra a imagem recortada do rosto
