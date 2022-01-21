5e874da2540421823c0a9c9bce07098113db1818
Alan Miranda <51090438+Ahmiranda@users.noreply.github.com>
2022-01-21 01:07:04 -0300

    o código tá rodando

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
-import cv2
 from PIL import Image
 import face_recognition as fr
-import dlib
-
-print(dlib.cuda.get_num_devices())
-
-def main(void):
-    #aquisição de imagem com a biblioteca face_recognition
-    #carrega a imagem na variável foto
-    foto = fr.load_image_file('pessoas-conhecidas/alan3.png')
-
-    #encontra faces usando o modelo HOG
-    face_locations = fr.face_locations(foto)
-    face_render = fr.face_encodings(foto)
-
-    #número de rostos encontrados
-    print('Existem {} pessoas nesta imagem'.format(len(face_locations)))
-
-    present_original_image = Image.fromarray(foto)
-    present_original_image.show(void, 'pessoa encontrada')
 
 
-    while not cv2.waitKey(50) & 0xFF == ord('q'):
-        #manipulação de imagem com a biblioteca pillow
+imagem = fr.load_image_file("pessoas-conhecidas/alan2wanessa2.jpg")
+faces = fr.face_locations(imagem)
+print('Foram encontradas {} pessoas'.format(len(faces)))
+for coordenadas in faces:
+    x, y, w, z = coordenadas
+    rostoEncontrado = imagem[x:w, y:z]
+    recorte_da_foto_original = Image.fromarray(rostoEncontrado)
+    recorte_da_foto_original.show()
